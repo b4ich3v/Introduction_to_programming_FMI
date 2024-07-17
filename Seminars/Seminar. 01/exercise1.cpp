16e71e276ee2b2a08cd28fc7a6838847952ec639
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 00:50:11 +0300

    Update exercise1.cpp

--- Seminars/Seminar. 01/exercise1.cpp
+++ Seminars/Seminar. 01/exercise1.cpp
@@ -1,11 +1,15 @@
+// Въвежда се цяло число. Отпечатайте 1, ако числото е четно и 0, ако е нечетно.
+
 #include <iostream>
 
 int main()
 {
     
-	int number;
+	int number = 0;
 	std::cin >> number;
 	bool isEven = number % 2 == 0;
 	std::cout << isEven;
-    
+	
+	return 0;
+	
 }
