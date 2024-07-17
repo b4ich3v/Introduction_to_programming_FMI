b93bf6a5c0f57b2cbf3272ce96bfb4a0040a72cf
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 00:45:34 +0300

    Added exercise2.cpp

---
+++ Seminars/Seminar. 01/exercise2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+
+	int number;
+	std::cin >> number;
+
+	int thirdDigit = number % 10;
+	number /= 10; 
+
+	int secondDigit = number % 10;
+	number /= 10; 
+
+	int firstDigit = number % 10;
+
+	int reversed = thirdDigit * 100 + secondDigit * 10 + firstDigit;
+	reversed++;
+
+	std::cout << reversed << std::endl;
+
+	return 0;
+
+}
