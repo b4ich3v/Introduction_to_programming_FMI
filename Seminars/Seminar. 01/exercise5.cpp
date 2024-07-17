934f32f3c1466f9b075ffbefe7b5cd934189a030
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 00:59:26 +0300

    Create exercise5.cpp

---
+++ Seminars/Seminar. 01/exercise5.cpp
@@ -0,0 +1,15 @@
+// Въвежа се цяло число. Да се отпечата без последната цифра.
+
+#include <iostream>
+
+int main()
+{
+  
+	int number = 0;
+	std::cin >> number;
+
+	std::cout << number / 10 << std::endl;
+
+	return 0;
+
+}
