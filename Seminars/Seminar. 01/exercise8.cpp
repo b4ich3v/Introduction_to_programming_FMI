4cea21459b33830557196cdc3c5ff42d1b625ae3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 01:09:29 +0300

    Create exercise8.cpp

---
+++ Seminars/Seminar. 01/exercise8.cpp
@@ -0,0 +1,18 @@
+// Въвеждат се две числа. Да се отпечата по-голямото от двете.
+
+#include <iostream>
+
+int main()
+{
+
+	int a = 0;
+	int b = 0;
+	std::cin >> a >> b;
+
+	bool isFirstBigger = a > b;
+
+	std::cout << a * isFirstBigger + b * !isFirstBigger;
+
+	return 0;
+
+}
