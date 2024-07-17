8f699aab851b59d94923570a114dd4df263112ba
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 00:54:08 +0300

    Create exercise3.cpp

---
+++ Seminars/Seminar. 01/exercise3.cpp
@@ -0,0 +1,16 @@
+// Въвежа се символ. Да се отпечата 1, ако е английска гласна буква, и 0, ако не е.
+
+#include <iostream>
+
+int main()
+{
+
+	char ch;
+	std::cin >> ch;
+
+	bool isVowel = ch == 'a' || ch == 'e' || ch == 'o' || ch == 'u' || ch == 'i'
+		|| ch == 'A' || ch == 'E' || ch == 'O' || ch == 'U' || ch == 'I';
+
+	std::cout << isVowel << std::endl;
+
+}
