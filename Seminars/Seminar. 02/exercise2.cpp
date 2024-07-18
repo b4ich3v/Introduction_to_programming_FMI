2837c3cdc6a927b09621b41a277626bdf7e3f5a8
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 19:41:01 +0300

    Update exercise2.cpp

--- Seminars/Seminar. 02/exercise2.cpp
+++ Seminars/Seminar. 02/exercise2.cpp
@@ -12,19 +12,19 @@ int main()
 	if (number < 100) 
 	{
 
-		std::cout << "Less than 100";
+		std::cout << "Less than 100" << std::endl;
 
 	}
 	else if (number >= 100 && number <= 200)
 	{
 
-		std::cout << "Between 100 and 200";
+		std::cout << "Between 100 and 200"<< std::endl;
 
 	}
 	else
 	{
 
-		std::cout << "Greater than 200";
+		std::cout << "Greater than 200"<< std::endl;
 
 	}
 
