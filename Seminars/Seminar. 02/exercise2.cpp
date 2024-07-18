f85d260e6e786e88f6e23b6d27beeeacb11b6f88
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 19:36:35 +0300

    Create exercise2.cpp

---
+++ Seminars/Seminar. 02/exercise2.cpp
@@ -0,0 +1,33 @@
+// (Between 100 and 200): Да се напише програма, която въвежда цяло число и проверява дали е под 100, 
+// между 100 и 200 или над 200. 
+
+#include <iostream>
+
+int main()
+{
+
+	int number = 0;
+	std::cin >> number;
+
+	if (number < 100) 
+	{
+
+		std::cout << "Less than 100";
+
+	}
+	else if (number >= 100 && number <= 200)
+	{
+
+		std::cout << "Between 100 and 200";
+
+	}
+	else
+	{
+
+		std::cout << "Greater than 200";
+
+	}
+
+	return 0;
+
+}
