661c5e130185caaf77cd877b7717a50e70bf23b1
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 19:32:52 +0300

    Update exercise1.cpp

--- Seminars/Seminar. 02/exercise1.cpp
+++ Seminars/Seminar. 02/exercise1.cpp
@@ -1,1 +1,27 @@
+// Да се напише програма, която въвежда цяло число отпечатава с думи кой ден от седмицата е.
 
+#include <iostream>
+
+int main()
+{
+
+	int number = 0;
+	std::cin >> number;
+
+	switch (number) 
+	{
+
+	case 1: std::cout << "Monday"; break;
+	case 2: std::cout << "Tuesday"; break;
+	case 3: std::cout << "Wednesday"; break;
+	case 4: std::cout << "Thursday"; break;
+	case 5: std::cout << "Friday"; break;
+	case 6: std::cout << "Saturday"; break;
+	case 7: std::cout << "Sunday"; break;
+	default: std::cout << "Invalid"; break;
+
+	}
+
+	return 0;
+
+}
