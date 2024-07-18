32dfe657f896e87298966c503f060947acd1631b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 20:24:18 +0300

    Create exercise5.cpp

---
+++ Seminars/Seminar. 03/exercise5.cpp
@@ -0,0 +1,51 @@
+// Напишете програма, която приема естествено число и отпечатва разбиването му на прости множители, сортирани възходящо.
+
+#include <iostream>
+
+int main() 
+{
+
+    int number = 0;
+    std::cin >> number;
+
+	for (int i = 2; i < number; i++)
+	{
+
+		int counter = 0;
+		bool isPrime = true;
+
+		for (int j = 2; j < i; j++)
+		{
+
+			if (i % j == 0)
+			{
+
+				isPrime = false;
+				break;
+
+			}
+
+		}
+		
+		int temp = number;
+
+		while (temp > 0 && temp % i == 0)
+		{
+
+			temp /= i;
+			counter++;
+
+		}
+
+		if (isPrime && (number % i == 0))
+		{
+
+			std::cout << i << "^" << counter << " ";
+
+		}
+
+	}
+
+    return 0;
+
+}
