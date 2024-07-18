8dea6d28a5a41ff809aebadb92dfd588f7c5eaf6
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 23:57:38 +0300

    Create exercise17.cpp

---
+++ Seminars/Seminar. 03/exercise17.cpp
@@ -0,0 +1,59 @@
+// Да се напише програма, която приема цяло положително число n и отпечатва рамка
+
+#include <iostream>
+
+int main() 
+{
+
+    int number = 0;
+    std::cin >> number;
+
+    if (number <= 0)
+    {
+
+        std::cout << "The number must be a positive integer" << std::endl;
+        return 0;
+
+    }
+
+    std::cout << "+";
+
+    for (int i = 0; i < number - 2; i++)
+    {
+
+        std::cout << " -";
+
+    }
+
+    std::cout << " +" << std::endl;
+
+    for (int i = 0; i < number - 2; i++) 
+    {
+
+        std::cout << "|";
+
+        for (int j = 0; j < number - 2; j++)
+        {
+
+            std::cout << "  ";
+
+        }
+
+        std::cout << " |" << std::endl;
+
+    }
+
+    std::cout << "+";
+
+    for (int i = 0; i < number - 2; i++)
+    {
+
+        std::cout << " -";
+
+    }
+
+    std::cout << " +" << std::endl;
+
+    return 0;
+
+}
