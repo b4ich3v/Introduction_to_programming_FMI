49ed2d07e074c9da3b3b00871e743e4953bdc9c1
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-19 00:04:09 +0300

    Create exercise18.cpp

---
+++ Seminars/Seminar. 03/exercise18.cpp
@@ -0,0 +1,45 @@
+// Да се напише програма, която приема цяло положително число n и отпечатва гира
+
+#include <iostream>
+
+int main() 
+{
+
+    int number = 0;
+    std::cin >> number;
+
+    if (number <= 0)
+    {
+
+        std::cout << "The number must be a positive integer." << std::endl;
+        return 0;
+
+    }
+
+    for (int i = 0; i < number; i++)
+    {
+     
+        for (int j = 0; j < number; j++) 
+        {
+
+            if (i == j || i == number - j - 1) 
+            {
+
+                std::cout << ". ";
+
+            } else 
+            {
+
+                std::cout << "  ";
+
+            }
+
+        }
+
+        std::cout << std::endl;
+
+    }
+
+    return 0;
+
+}
