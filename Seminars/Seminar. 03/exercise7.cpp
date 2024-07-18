bd2c843df0f095f00172ee95cde50a0f3e8faa10
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 20:35:22 +0300

    Create exercise7.cpp

---
+++ Seminars/Seminar. 03/exercise7.cpp
@@ -0,0 +1,65 @@
+// Напишете програма, която приема нечетно естествено число и отпечатва пясъчен часовник.
+
+#include <iostream>
+
+int main() 
+{
+
+    int number = 0;
+    std::cin >> number;
+
+    if (number % 2 == 0 || number <= 0)
+    {
+
+        std::cout << "Invalid number" << std::endl;
+        return 0;
+
+    }
+
+    std::cout << std::endl;
+
+    for (int i = 0; i <= number / 2; i++) 
+    {
+
+        for (int j = 0; j < i; j++) 
+        {
+
+            std::cout << " ";
+
+        }
+
+        for (int j = 0; j < number - 2 * i; j++) 
+        {
+
+            std::cout << "*";
+
+        }
+
+        std::cout << std::endl;
+
+    }
+
+    for (int i = number / 2 - 1; i >= 0; i--) 
+    {
+
+        for (int j = 0; j < i; j++)
+        {
+
+            std::cout << " ";
+
+        }
+
+        for (int j = 0; j < number - 2 * i; j++)
+        {
+
+            std::cout << "*";
+
+        }
+
+        std::cout << std::endl;
+
+    }
+
+    return 0;
+
+}
