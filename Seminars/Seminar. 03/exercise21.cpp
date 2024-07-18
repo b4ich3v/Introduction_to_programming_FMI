1c3ea345b2428824a8332f9934c6031ae363ba2b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-19 00:24:40 +0300

    Create exercise21.cpp

---
+++ Seminars/Seminar. 03/exercise21.cpp
@@ -0,0 +1,89 @@
+// Да се напише програма, която приема цяло положително число n и отпечатва килим
+
+#include <iostream>
+
+int main() 
+{
+
+    int number = 0;
+    std::cin >> number;
+
+    if (number <= 0) 
+    {
+
+        std::cout << "The number must be a positive integer" << std::endl;
+        return 0;
+
+    }
+
+    int size = 2 * number + 1;
+
+    for (int i = 0; i < size; i++) 
+    {
+
+        std::cout << "#";
+
+    }
+
+    std::cout << std::endl;
+
+    for (int i = 1; i <= number; i++) 
+    {
+
+        std::cout << "#";
+
+        for (int j = 0; j < number; j++) 
+        {
+
+            std::cout << (j + i - 1) % number + 1;
+
+        }
+
+        std::cout << "#";
+        std::cout << std::endl;
+
+    }
+
+    std::cout << "# ";
+
+    for (int i = 0; i < number - 1; i++) 
+    {
+
+        std::cout << "X ";
+
+    }
+
+    std::cout << "X #";
+    std::cout << std::endl;
+
+
+    for (int i = number; i >= 1; i--) 
+    {
+
+        std::cout << "#";
+
+        for (int j = 0; j < number; j++) 
+        {
+
+            std::cout << (j + i - 1) % number + 1;
+
+        }
+
+        std::cout << "#";
+        std::cout << std::endl;
+
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+
+        std::cout << "#";
+
+    }
+
+    std::cout << std::endl;
+
+
+    return 0;
+
+}
