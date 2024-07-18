4fd94e3d7c2f82b1e63a622d80d8afb1ea5b5ca3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-18 19:51:51 +0300

    Create exercise7.cpp

---
+++ Seminars/Seminar. 02/exercise7.cpp
@@ -0,0 +1,36 @@
+// Да се напише програма, която при подадени радиус r и двумерна точка (х, у) 
+// проверява дали точката се намира вътре/извън или по контура на окръжност с център (0,0).
+
+#include <iostream>
+
+int main()
+{
+
+	int x = 0;
+	int y = 0;
+	int radius = 0;
+
+	std::cin >> x >> y >> radius;
+
+	if (x * x + y * y < radius * radius)
+	{
+
+		std::cout << "In the circle";
+
+	}
+	else if (x * x + y * y == radius * radius)
+	{
+
+		std::cout << "On the circle";
+
+	}
+	else
+	{
+
+		std::cout << "Out of the circle";
+
+	}
+
+	return 0;
+
+}
