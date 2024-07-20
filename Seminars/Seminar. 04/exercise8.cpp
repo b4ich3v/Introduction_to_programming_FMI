99a6af340885404173d4ca366e955553cf958bcd
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-20 16:42:03 +0300

    Create exercise8.cpp

---
+++ Seminars/Seminar. 04/exercise8.cpp
@@ -0,0 +1,37 @@
+// Напишете функция, която приема неотрицателно числа n и k връща log_k(n) (закръглено надолу)
+
+#include <iostream>
+
+int logBaseK(int n, int k)
+{
+
+    if (k <= 1) 
+    {
+
+        std::cout << "Base k must be greater than 1";
+        return -1; 
+
+    }
+
+    int result = 0;
+
+    while (n >= k) 
+    {
+
+        n /= k;
+        result++;
+
+    }
+
+    return result;
+
+}
+
+int main()
+{
+
+	std::cout << logBaseK(90, 3);
+
+	return 0;
+
+}
