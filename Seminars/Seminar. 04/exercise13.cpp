51c59b307d5d1b240e7539146a96e2b32f3f9195
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-20 17:02:21 +0300

    Create exercise13.cpp

---
+++ Seminars/Seminar. 04/exercise13.cpp
@@ -0,0 +1,66 @@
+// Да се напише функция, която приема две естествени числа и връща ново число, което е тяхната конкатенация.
+
+#include <iostream>
+
+int countOfDigits(int number) 
+{
+
+	if(number < 0)
+	{
+
+		std::cout << "Error";
+		return -1;
+
+	}
+
+	int count = 0;
+
+	while (number != 0) 
+	{
+
+		count++;
+		number /= 10;
+
+	}
+
+	return count;
+
+}
+
+int concatOfTwoNumbers(int n, int k)
+{
+
+	int countOfDigitsN = countOfDigits(n);
+	int countOfDigitsK = countOfDigits(k);
+	int newCountOfDigits = countOfDigitsN + countOfDigitsK - 1;
+
+	if (countOfDigitsK == -1 || countOfDigitsN == -1)
+	{
+
+		return -1;
+
+	}
+
+	int iter = 1;
+
+	for (int i = 0; i < newCountOfDigits - 1; i++)
+	{
+
+		iter *= 10;
+
+	}
+
+	int result = n * iter + k;
+
+	return result;
+
+}
+
+int main()
+{
+
+	std::cout << concatOfTwoNumbers(19, 32);
+
+	return 0;
+
+}
