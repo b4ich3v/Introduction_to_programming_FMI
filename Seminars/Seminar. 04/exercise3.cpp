33a09c65e75ed7b174c571f09266fa45e7e717af
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-20 16:13:20 +0300

    Create exercise3.cpp

---
+++ Seminars/Seminar. 04/exercise3.cpp
@@ -0,0 +1,57 @@
+// Напишете функция, която приема неотрицателно число и връща дали числото е палиндром.
+
+#include <iostream>
+
+int inversedNumber(int number) 
+{
+
+	int result = 0;
+	int iter = 1;
+	int countOfDigits = 0;
+	int originalNumber = number;
+
+	while (number != 0) 
+	{
+
+		number /= 10;
+		countOfDigits++;
+
+	}
+
+	for (int i = 0; i < countOfDigits - 1; i++)
+	{
+
+		iter *= 10;
+
+	}
+
+	number = originalNumber;
+
+	while (number != 0)
+	{
+
+		result = result + iter * (number % 10);
+		iter /= 10;
+		number /= 10;
+
+	}
+
+	return result;
+
+}
+
+bool isNumberPalindrome(int number) 
+{
+
+	return number == inversedNumber(number);
+
+}
+
+int main()
+{
+
+	std::cout<< isNumberPalindrome(121);
+
+    return 0;
+
+}
