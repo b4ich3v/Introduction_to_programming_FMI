5038a462724cd3746917deecfdd68801491f8efb
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-20 16:18:32 +0300

    Create exercise4.cpp

---
+++ Seminars/Seminar. 04/exercise4.cpp
@@ -0,0 +1,87 @@
+// Напишете функция, която приема неотрицателни числа n и k и връща дали k е суфикс на n.
+
+#include <iostream>
+
+int inversedNumber(int number) 
+{
+
+	int result = 0;
+	int iter = 1;
+	int countOfDigits = 0;
+	int originalNumber = number;
+
+	while (number != 0) 
+	{
+
+		number /= 10;
+		countOfDigits++;
+
+	}
+
+	for (int i = 0; i < countOfDigits - 1; i++)
+	{
+
+		iter *= 10;
+
+	}
+
+	number = originalNumber;
+
+	while (number != 0)
+	{
+
+		result = result + iter * (number % 10);
+		iter /= 10;
+		number /= 10;
+
+	}
+
+	return result;
+
+}
+
+bool isSufix(int n ,int k) 
+{
+
+	int countOfDigitsK = 0;
+	int originalNumberK = k;
+	int iter = 1;
+
+	while (k != 0)
+	{
+
+		k /= 10;
+		countOfDigitsK++;
+
+	}
+
+	k = originalNumberK;
+
+	for (int i = 0; i < countOfDigitsK - 1; i++)
+	{
+
+		iter *= 10;
+
+	}
+
+	n = inversedNumber(n);
+	
+	for (int i = 0; i < countOfDigitsK - 1; i++)
+	{
+
+		n /= 10;
+
+	}
+
+	return n == k;
+
+}
+
+int main()
+{
+
+	std::cout<< isSufix(288, 88);
+
+    return 0;
+
+}
