87a6f5d2cd080ad4ffda833aa7455e8711964ada
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-20 16:24:46 +0300

    Create exercise5.cpp

---
+++ Seminars/Seminar. 04/exercise5.cpp
@@ -0,0 +1,59 @@
+// Напишете функция, която приема неотрицателни числа n и k и връща дали k е префикс на n.
+
+#include <iostream>
+
+int countOfDigits(int number)
+{
+
+	int count = 0;
+
+	while(number != 0)
+	{
+
+		count++;
+		number /= 10;
+
+	}
+
+	return count;
+
+}
+
+bool isPrefix(int n ,int k) 
+{
+
+	int countOfDigitsK = countOfDigits(k);
+	int originalNumberK = k;
+
+	int countOfDigitsN = countOfDigits(n);
+	int originalNumberN = n;
+
+
+	if(countOfDigitsN < countOfDigitsK)
+	{
+
+		return false;
+
+	}
+
+	int iter = countOfDigitsN - countOfDigitsK;
+
+	for (int i = 0; i < iter; i++)
+	{
+
+		n /= 10;
+
+	}
+
+	return n == k;
+
+}
+
+int main()
+{
+
+	std::cout<< isPrefix(288, 28);
+
+    return 0;
+
+}
