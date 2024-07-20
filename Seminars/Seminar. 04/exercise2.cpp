ddbcbd33f9348db9083778f4644f470cc7e866a4
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-20 16:11:18 +0300

    Create exercise2.cpp

---
+++ Seminars/Seminar. 04/exercise2.cpp
@@ -0,0 +1,50 @@
+// Напишете функция, която приема неотрицателно число n и връща числото обърнато
+
+#include <iostream>
+
+int inversedNumber(int number) 
+{
+
+	int result = 0;
+	int iter = 1;
+	int countOfDigits = 0;
+	int originalNumber = number;
+
+	while (number != 0) 
+	{
+
+		number /= 10;
+		countOfDigits++;
+
+	}
+
+	for (int i = 0; i < countOfDigits - 1; i++)
+	{
+
+		iter *= 10;
+
+	}
+
+	number = originalNumber;
+
+	while (number != 0)
+	{
+
+		result = result + iter * (number % 10);
+		iter /= 10;
+		number /= 10;
+
+	}
+
+	return result;
+
+}
+
+int main()
+{
+
+	std::cout<<inversedNumber(123);
+
+    return 0;
+
+}
