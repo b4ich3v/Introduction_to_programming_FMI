58a2edc0a39e52e346c5916b061e583c1d09b55e
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-20 16:34:58 +0300

    Create exercise6.cpp

---
+++ Seminars/Seminar. 04/exercise6.cpp
@@ -0,0 +1,69 @@
+// Напишете функция, която приема неотрицателни числа n и k и връща дали k е инфикс в n.
+
+#include <iostream>
+
+int countOfDigits(int number)
+{
+
+	int count = 0;
+
+	while (number != 0)
+	{
+
+		count++;
+		number /= 10;
+
+	}
+
+	return count;
+
+}
+
+bool isInfix(int n, int k)
+{
+
+	int countOfDigitsK = countOfDigits(k);
+	int countOfDigitsN = countOfDigits(n);
+
+	if (countOfDigitsN < countOfDigitsK)
+	{
+
+		return false;
+
+	}
+
+	int iter = 1;
+
+	for (int i = 0; i < countOfDigitsK; i++)
+	{
+
+		iter *= 10;
+
+	}
+
+	for (int i = 0; i <= countOfDigitsN - countOfDigitsK; i++)
+	{
+
+		if (n % iter == k)
+		{
+
+			return true;
+
+		}
+
+		n /= 10;
+
+	}
+
+	return false;
+
+}
+
+int main()
+{
+
+	std::cout << isInfix(2831, 83);
+
+	return 0;
+
+}
