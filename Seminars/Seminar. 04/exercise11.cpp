f44660e2f3926c587188b6e3b321255f97991771
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-20 16:51:24 +0300

    Create exercise11.cpp

---
+++ Seminars/Seminar. 04/exercise11.cpp
@@ -0,0 +1,38 @@
+// Напишете функция, която приема цяло число и връща броя на цифрите му.
+
+#include <iostream>
+
+int countOfDigits(int number)
+{
+
+	if (number < 0) 
+	{
+
+		std::cout << "Error";
+		return - 1;
+
+	}
+
+	int count = 0;
+
+	while (number != 0) 
+	{
+
+		count++;
+		number /= 10;
+
+	}
+
+	return count;
+
+}
+
+
+int main()
+{
+
+	std::cout << countOfDigits(123);
+
+	return 0;
+
+}
