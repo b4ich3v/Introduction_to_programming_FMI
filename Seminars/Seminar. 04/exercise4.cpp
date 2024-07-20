cf6f226ddf35b4d354ebe8d2ebb69194f6fb46d0
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-20 16:29:56 +0300

    Update exercise4.cpp

--- Seminars/Seminar. 04/exercise4.cpp
+++ Seminars/Seminar. 04/exercise4.cpp
@@ -2,18 +2,26 @@
 
 #include <iostream>
 
-int inversedNumber(int number) 
+int inversedNumber(int number)
 {
 
 	int result = 0;
+	int iter = 1;
 	int countOfDigits = 0;
 	int originalNumber = number;
 
-	while (number != 0) 
+	while (number != 0)
 	{
 
 		number /= 10;
 		countOfDigits++;
+
+	}
+
+	for (int i = 0; i < countOfDigits - 1; i++)
+	{
+
+		iter *= 10;
 
 	}
 
@@ -32,32 +40,38 @@ int inversedNumber(int number)
 
 }
 
-bool isSufix(int n ,int k) 
+int countOfDigits(int number)
 {
 
-	int countOfDigitsK = 0;
-	int originalNumberK = k;
-	int iter = 1;
+	int count = 0;
 
-	while (k != 0)
+	while (number != 0)
 	{
 
-		k /= 10;
-		countOfDigitsK++;
+		count++;
+		number /= 10;
 
 	}
 
-	k = originalNumberK;
+	return count;
 
-	for (int i = 0; i < countOfDigitsK - 1; i++)
+}
+
+bool isSufix(int n, int k)
+{
+
+	int countOfDigitsK = countOfDigits(k);
+	int countOfDigitsN = countOfDigits(n);
+
+	if (countOfDigitsN < countOfDigitsK)
 	{
 
-		iter *= 10;
+		return false;
 
 	}
 
 	n = inversedNumber(n);
-	
+
 	for (int i = 0; i < countOfDigitsK - 1; i++)
 	{
 
@@ -72,8 +86,8 @@ bool isSufix(int n ,int k)
 int main()
 {
 
-	std::cout<< isSufix(288, 88);
+	std::cout << isSufix(288, 88);
 
-    return 0;
+	return 0;
 
 }
