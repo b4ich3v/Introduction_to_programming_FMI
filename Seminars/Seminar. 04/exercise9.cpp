0f4a993ef87bd96bf3154dc4e692268a45f1ee14
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-20 16:47:49 +0300

    Create exercise9.cpp

---
+++ Seminars/Seminar. 04/exercise9.cpp
@@ -0,0 +1,31 @@
+//  Напишете функция, която приема 4 цели числа - a,b,c,d и връща дали точката <a,b> и точката <c,d> лежат на една окръжност с център <0,0>.
+
+#include <iostream>
+
+bool areOnTheCircle(int a, int b, int c, int d) 
+{
+
+	int xA = a;
+	int yA = b;
+
+	int xB = c;
+	int yB = d;
+
+	int lenA = 0;
+	int lenB = 0;
+
+	int lenAPower2 = xA * xA + yA * yA;
+	int lenBPower2 = xB * xB + yB * yB;
+
+	return lenAPower2 == lenBPower2;
+
+}
+
+int main()
+{
+
+	std::cout << areOnTheCircle(1, 0, 1, 0);
+
+	return 0;
+
+}
