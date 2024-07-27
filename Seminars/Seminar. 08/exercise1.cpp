d659e410c6f3719201fd1923f78e4ce0ea42127d
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-27 18:01:12 +0300

    Create exercise1.cpp

---
+++ Seminars/Seminar. 08/exercise1.cpp
@@ -0,0 +1,83 @@
+// Напишете функция, която приема матрица NxN и транспонира матрицата.
+
+#include <iostream>
+
+const int MAX_SIZE = 100;  
+
+void swap(int& a , int& b)
+{
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+}
+
+void transposeMatrix(int matrix[MAX_SIZE][MAX_SIZE], int size)
+{
+  
+    for (int i = 0; i < size; i++)
+    {
+
+        for (int j = i + 1; j < size; j++)
+        {
+            
+            swap(matrix[i][j], matrix[j][i]);
+
+        }
+
+    }
+
+}
+
+void printMatrix(int matrix[MAX_SIZE][MAX_SIZE], int N)
+{
+
+    for (int i = 0; i < N; ++i) 
+    {
+
+        for (int j = 0; j < N; ++j) 
+        {
+
+            std::cout << matrix[i][j] << " ";
+
+        }
+
+        std::cout << std::endl;
+
+    }
+
+}
+
+int main() 
+{
+
+    int size = 0;
+    std::cin >> size;
+
+    if (size > MAX_SIZE) 
+    {
+        std::cout << "Error" << std::endl;
+        return 0;
+
+    }
+
+    int matrix[MAX_SIZE][MAX_SIZE] = {0};
+
+    for (int i = 0; i < size; i++) 
+    {
+
+        for (int j = 0; j < size; j++)
+        {
+
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    transposeMatrix(matrix, size);
+    std::cout << std::endl;
+    printMatrix(matrix, size);
+
+    return 0;
+
+}
