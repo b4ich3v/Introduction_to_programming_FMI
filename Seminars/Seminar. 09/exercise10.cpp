e7626c6a75507cf64371426a1bb373b2489bc33e
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-27 23:47:02 +0300

    Create exercise10.cpp

---
+++ Seminars/Seminar. 09/exercise10.cpp
@@ -0,0 +1,151 @@
+// Напишете функция, която приема 3 стринга - text, where и what и земства в text всяко срещане на where с what. 
+// Да се реши без допълнителна памет(in - place).
+
+#include <iostream>
+
+int customStrlen(const char* string)
+{
+
+    if (string == nullptr)
+    {
+
+        return -1;
+
+    }
+
+    int size = 0;
+
+    while (*string != '\0')
+    {
+
+        string++;
+        size++;
+
+    }
+
+    return size;
+
+}
+
+int customStrncmp(const char* string1, const char* string2, int number) 
+{
+
+    int index = 0;
+
+    while (index < number && string1[index] != '\0' && string2[index] != '\0')
+    {
+
+        if (string1[index] != string2[index]) 
+        {
+
+            return (unsigned char)string1[index] - (unsigned char)string2[index];
+
+        }
+
+        index++;
+
+    }
+
+    if (index < number) 
+    {
+
+        if (string1[index] != string2[index]) 
+        {
+
+            return (unsigned char)string1[index] - (unsigned char)string2[index];
+
+        }
+
+    }
+
+    return 0;
+
+}
+
+void replaceInPlace(char* text, const char* where, const char* what) 
+{
+
+    int textLen = customStrlen(text);
+    int whereLen = customStrlen(where);
+    int whatLen = customStrlen(what);
+
+    if (whereLen == 0) 
+    {
+
+        return;
+
+    }
+
+    for (int i = 0; i <= textLen - whereLen; )
+    {
+        
+        if (customStrncmp(&text[i], where, whereLen) == 0)
+        {
+            
+            if (whatLen > whereLen) 
+            {
+
+                int shiftLen = whatLen - whereLen;
+
+                for (int j = textLen; j >= i + whereLen; j--) 
+                {
+
+                    text[j + shiftLen] = text[j];
+
+                }
+
+                textLen += shiftLen;
+
+            }
+
+            if (whatLen < whereLen) 
+            {
+
+                int shiftLen = whereLen - whatLen;
+
+                for (int j = i + whereLen; j <= textLen; j++) 
+                {
+
+                    text[j - shiftLen] = text[j];
+
+                }
+
+                textLen -= shiftLen;
+
+            }
+
+            for (int k = 0; k < whatLen; k++) 
+            {
+
+                text[i + k] = what[k];
+
+            }
+
+            i += whatLen;
+
+        }
+        else 
+        {
+
+            i++;
+
+        }
+
+    }
+
+}
+
+int main() 
+{
+
+    char text[100] = "Hello world! Hello everyone!";
+    const char* where = "Hello";
+    const char* what = "Hi";
+
+    replaceInPlace(text, where, what);
+
+    std::cout << text << std::endl;
+
+    return 0;
+
+}
