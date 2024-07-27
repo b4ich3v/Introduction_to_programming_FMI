36355034ccad7747af494da02adc751531d70251
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-27 18:06:17 +0300

    Create exercise2.cpp

---
+++ Seminars/Seminar. 08/exercise2.cpp
@@ -0,0 +1,139 @@
+// Напишете функция, която приема матрица NxN и проверява дали релацията, която представлява матрицата, е релация на еквивалентност.
+
+#include <iostream>
+
+const int MAX_SIZE = 100;  
+
+bool isReflexive(int matrix[MAX_SIZE][MAX_SIZE], int size)
+{
+
+    for (int i = 0; i < size; i++) 
+    {
+
+        if (matrix[i][i] != 1)
+        {
+
+            return false;
+
+        }
+
+    }
+
+    return true;
+
+}
+
+bool isSymmetric(int matrix[MAX_SIZE][MAX_SIZE], int size)
+{
+
+    for (int i = 0; i < size; i++) 
+    {
+
+        for (int j = 0; j < size; j++) 
+        {
+
+            if (matrix[i][j] != matrix[j][i]) 
+            {
+
+                return false;
+
+            }
+
+        }
+
+    }
+
+    return true;
+
+}
+
+bool isTransitive(int matrix[MAX_SIZE][MAX_SIZE], int size)
+{
+
+    for (int i = 0; i < size; i++) 
+    {
+
+        for (int j = 0; j < size; j++) 
+        {
+
+            if (matrix[i][j] == 1) 
+            {
+
+                for (int k = 0; k < size; k++) 
+                {
+
+                    if (matrix[j][k] == 1 && matrix[i][k] != 1) 
+                    {
+
+                        return false;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+    return true;
+
+}
+
+bool isEquivalenceRelation(int matrix[MAX_SIZE][MAX_SIZE], int N)
+{
+
+    return isReflexive(matrix, N) && isSymmetric(matrix, N) && isTransitive(matrix, N);
+
+}
+
+void printMatrix(int matrix[MAX_SIZE][MAX_SIZE], int N)
+{
+
+    for (int i = 0; i < N; ++i) 
+    {
+
+        for (int j = 0; j < N; ++j) 
+        {
+
+            std::cout << matrix[i][j] << " ";
+
+        }
+
+        std::cout << std::endl;
+
+    }
+
+}
+
+int main() 
+{
+
+    int size = 0;
+    std::cin >> size;
+
+    if (size > MAX_SIZE) 
+    {
+        std::cout << "Error" << std::endl;
+        return 0;
+
+    }
+
+    int matrix[MAX_SIZE][MAX_SIZE] = {0};
+
+    for (int i = 0; i < size; i++) 
+    {
+
+        for (int j = 0; j < size; j++)
+        {
+
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    std::cout << isEquivalenceRelation(matrix,size);
+
+    return 0;
+
+}
