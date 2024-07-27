8aed0e3f8ad07fe2de3bb328ea31cfd8b1ea2924
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-27 16:27:25 +0300

    Create exercise3.cpp

---
+++ Seminars/Seminar. 05/exercise3.cpp
@@ -0,0 +1,46 @@
+// Да се напише функция, която приема масив и връща най-големият общ делител на числата в масива.
+
+#include <iostream>
+
+int GCD(int n, int k)
+{
+
+	while (k != 0)
+	{
+
+		int temp = k;
+		k = n % k;
+		n = temp;
+
+	}
+
+	return n;
+
+}
+
+int GcdOfArray(int* array, int size)
+{
+
+	int gcd = array[0];
+
+	for (int i = 1; i < size; i++)
+	{
+
+		gcd = GCD(gcd, array[i]);
+
+	}
+
+	return gcd;
+
+}
+
+int main()
+{
+
+	int array[5] = { 9 ,18 ,6 ,12 };
+
+	std::cout << GcdOfArray(array, 4);
+
+	return 0;
+
+}
