125a86cdea90d536a213af0d36735d9e9e7631b3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-27 23:36:33 +0300

    Create exercise6.cpp

---
+++ Seminars/Seminar. 09/exercise6.cpp
@@ -0,0 +1,85 @@
+// Напишете функция, която проверява дали един низ се съдържа в друг низ.
+
+#include <iostream>
+#include <climits> 
+
+int customStrlen(const char* string)
+{
+
+	if (string == nullptr)
+	{
+
+		return -1;
+
+	}
+
+	int size = 0;
+
+	while (*string != '\0')
+	{
+
+		string++;
+		size++;
+
+	}
+
+	return size;
+
+}
+
+bool isPrefix(const char* pattern, const char* text)
+{
+
+	while (*text != '\0' && *pattern != '\0')
+	{
+
+		if (*text != *pattern) 
+		{
+
+			return false;
+
+		}
+		
+		text++;
+		pattern++;
+
+	}
+
+	return *pattern == '\0';
+
+}
+
+bool searchInText(const char* text, const char* pattern)
+{
+
+	int textLen = customStrlen(text);
+	int patternLen = customStrlen(pattern);
+
+	while (patternLen <= textLen)
+	{
+
+		if (isPrefix(pattern, text))
+		{
+
+			return true;
+
+		}
+	
+		text++; 
+		textLen--;
+
+	}
+
+	return false;
+
+}
+
+int main()
+{
+
+	char string[] = "My name is Yoan Baychev";
+	char pattern[] = "Yoan";
+
+	std::cout << searchInText(string, pattern);
+
+}
