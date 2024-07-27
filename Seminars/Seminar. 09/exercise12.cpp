34c69b29aa342b4f4a7fbd8315a1594a8d355ace
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-27 23:56:38 +0300

    Create exercise12.cpp

---
+++ Seminars/Seminar. 09/exercise12.cpp
@@ -0,0 +1,54 @@
+// Напишете функция, която приема стринг и връща броя на думите в него. 
+// (Думите са разделени с произволен брой интервали, табулации и препинателни знаци)
+
+#include <iostream>
+
+int countOfWords(char* string)
+{
+
+    int count = 0;
+    bool inWord = false;
+
+    while (*string != '\0')
+    {
+
+        while (*string == '\t' || *string == ' ' && *string != '\0')
+        {
+
+            string++;
+
+        }
+
+        if (*string != '\t' && *string != ' ' && *string != '\0')
+        {
+
+            inWord = true;
+            count++;
+
+        }
+
+        inWord = false;
+
+        while (*string != '\t' && *string != ' ' && *string != '\0')
+        {
+
+            string++;
+
+        }
+
+    }
+
+    return count;
+
+}
+
+int main() 
+{
+
+    char string[] = "Me? Why always me?";
+
+    std::cout << countOfWords(string) << std::endl;
+
+    return 0;
+
+}
