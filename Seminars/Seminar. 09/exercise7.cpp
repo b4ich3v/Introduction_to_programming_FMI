6a100852476d5eb392ff145dc742a87fcb31c063
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-27 23:39:21 +0300

    Create exercise7.cpp

---
+++ Seminars/Seminar. 09/exercise7.cpp
@@ -0,0 +1,38 @@
+// Напишете функция, която намира броя на срещанията на конкретен символ в текст.
+
+#include <iostream>
+
+int countOfApp(char* string, char ch)
+{
+
+	int cout = 0;
+
+	while (*string != '\0') 
+	{
+
+		if (*string == ch)
+		{
+
+			cout++;
+
+		}
+
+		string++;
+
+	}
+
+	return cout;
+
+}
+
+int main()
+{
+
+	char string[] = "My name is Yoan Baychev";
+	char ch = 'a';
+
+	std::cout << countOfApp(string, ch);
+
+	return 0;
+
+}
