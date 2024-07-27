f06505bb85c516196303f1c9957fed744dc775f6
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-28 01:13:18 +0300

    Create exercise2.cpp

---
+++ Seminars/Seminar. 10/exercise2.cpp
@@ -0,0 +1,77 @@
+// Да се напише функция, която приема стринг и връща два стринга с ТОЧНА големина. 
+// Първият да бъде съставен само от малките букви, а другият да бъде съставен само от главните букви.
+
+#include <iostream>
+
+void splitByCase(const char* string, char*& lower, char*& upper)
+{
+
+    int lowercaseCount = 0;
+    int uppercaseCount = 0;
+    int length = strlen(string);
+
+    for (int i = 0; i < length; i++)
+    {
+
+        if (string[i] >= 'a' && string[i] <= 'z')
+        {
+
+            lowercaseCount++;
+
+        }
+        else if (string[i] >= 'A' && string[i] <= 'Z')
+        {
+
+            uppercaseCount++;
+
+        }
+
+    }
+
+    lower = new char[lowercaseCount + 1];
+    upper = new char[uppercaseCount + 1];
+
+    int lowercaseIndex = 0;
+    int uppercaseIndex = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+
+        if (string[i] >= 'a' && string[i] <= 'z')
+        {
+
+            lower[lowercaseIndex++] = string[i];
+
+        }
+        else if (string[i] >= 'A' && string[i] <= 'Z')
+        {
+
+            upper[uppercaseIndex++] = string[i];
+
+        }
+
+    }
+
+    lower[lowercaseIndex] = '\0';
+    upper[uppercaseIndex] = '\0';
+
+}
+
+int main()
+{
+
+    const char* string = "My name is Yoan Baychev.";
+
+    char* lowercase = nullptr;
+    char* uppercase = nullptr;
+
+    splitByCase(string, lowercase, uppercase);
+
+    std::cout << lowercase << std::endl;
+    std::cout << uppercase << std::endl;
+
+    delete[] lowercase;
+    delete[] uppercase;
+
+    return 0;
+}
