1667d948796e079e1a2b765138af58493f93ca4c
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-29 00:04:24 +0300

    Create exercise2.cpp

---
+++ Seminars/Seminar. 13/exercise2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int fibonacci(int number)
+{
+
+    if (number == 0)
+    {
+
+        return 0;
+
+    }
+
+    if (number == 1) 
+    {
+
+        return 1;
+
+    }
+
+    return fibonacci(number - 1) + fibonacci(number - 2);
+
+}
+
+int main()
+{
+
+	std::cout << fibonacci(13);
+
+	return 0;
+
+}
