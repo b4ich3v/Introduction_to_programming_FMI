f8aa0165e6a16e21c63bf3b902c93c538064e898
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-28 22:34:58 +0300

    Update exercise8.cpp

--- Seminars/Seminar. 09/exercise8.cpp
+++ Seminars/Seminar. 09/exercise8.cpp
@@ -1,6 +1,3 @@
-// Напишете функция, която приема 3 стринга - text, where и what и земства в text всяко срещане на where с what. 
-// Да се реши без допълнителна памет(in - place).
-
 #include <iostream>
 
 int customStrlen(const char* string)
