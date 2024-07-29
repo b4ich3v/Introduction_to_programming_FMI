503bee2b10b469d67041973865f88d496ccf1412
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-29 17:28:51 +0300

    Update exercise6.cpp

--- Practicums/Practicum. 02/exercise6.cpp
+++ Practicums/Practicum. 02/exercise6.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+
+int main() 
+{
+   
+    int number = 0;
+    std::cin >> number;
+
+    if (number == 0) 
+    {
+
+        std::cout << "zero" << std::endl;
+
+    }
+    else if (number % 2 == 0) 
+    {
+
+        std::cout << "even" << std::endl;
+
+    }
+    else 
+    {
+
+        std::cout << "odd" << std::endl;
+
+    }
+
+    return 0;
+
+}
 
