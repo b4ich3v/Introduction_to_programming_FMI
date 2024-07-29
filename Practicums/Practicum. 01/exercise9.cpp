fb98f531cb9e4eae0cef8f487277a9f54a3dcd95
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-29 17:00:49 +0300

    Update exercise9.cpp

--- Practicums/Practicum. 01/exercise9.cpp
+++ Practicums/Practicum. 01/exercise9.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+
+int main() 
+{
+    
+    int number1 = 0;
+    int number2 = 0;
+
+    std::cin >> number1 >> number2;
+
+    number1 = number1 ^ number2;
+    number2 = number1 ^ number2;
+    number1 = number1 ^ number2;
+
+    std::cout << number1 << " " << number2;
+
+    return 0;
+
+}
 
