e3e5c28147ae205b6a2f1ddcb7fff8e4f46972b8
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-29 16:44:50 +0300

    Update exercise1.cpp

--- Practicums/Practicum. 01/exercise1.cpp
+++ Practicums/Practicum. 01/exercise1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+
+int main() 
+{
+
+    int number1 = 0;
+    int number2 = 0;
+
+    std::cin >> number1 >> number2;
+
+    int remainder = number1 % number2;
+    int especially = number1 / number2;
+
+    std::cout << remainder << " " << especially;
+
+    return 0;
+
+}
 
