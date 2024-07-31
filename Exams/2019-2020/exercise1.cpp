bee6e6d56d39780a7e80ea952fc1069043a4c715
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-07-31 16:54:54 +0300

    Update exercise1.cpp

--- Exams/2019-2020/exercise1.cpp
+++ Exams/2019-2020/exercise1.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
 
+int AnalyzeArray(int arr[], int l)
+{
+
+	if (l < 0) 
+	{
+
+		return -1;
+
+	}
+
+	int sum = 0;
+
+
+	for (int i = 0; i < l; i++)
+	{
+
+		sum += arr[i];
+
+	}
+
+	int currSum = 0;
+
+	for (int i = 0; i < l; i++)
+	{
+
+		currSum += arr[i];
+
+		if (sum - currSum == currSum) 
+		{
+
+			return 1;
+			break;
+
+		}
+
+	}
+
+	return 0;
+
+}
+
+int main() 
+{
+
+	int arr[] = { 1,6,1,0,5,199 };
+	std::cout << AnalyzeArray(arr, 6);
+
+	return 0;
+
+}
