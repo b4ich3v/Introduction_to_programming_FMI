f8f3ac1e7860c324957f9aa034dbaa5ab8054dca
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-08-02 21:53:03 +0300

    Create exercise10.cpp

---
+++ Exams/2020-2021/exercise10.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+int** mergearrays(int* array1, int* array2, int m)
+{
+
+    if (array1 == nullptr || array2 == nullptr || m < 0)
+    {
+
+        std::cout << "Error";
+        return nullptr;
+
+    }
+
+    int** toReturn = new int* [2];
+
+    toReturn[0] = new int[m];
+    toReturn[1] = new int[m];
+
+    for (int i = 0; i < m; i++)
+    {
+
+        toReturn[0][i] = array1[i];
+        toReturn[1][i] = array2[i];
+
+    }
+
+    return toReturn;
+
+}
+
+int main()
+{
+
+    int array1[] = { 3, 5, 11, 2, 9 };
+    int array2[] = { 1, 4, 32, 71, 5 };
+
+    int** result = mergearrays(array1, array2, 5);
+
+    for (int i = 0; i < 2; i++)
+    {
+
+        for (int j = 0; j < 5; j++)
+        {
+
+            std::cout << result[i][j] << " ";
+
+        }
+
+        std::cout << std::endl;
+
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+
+        delete result[i];
+
+    }
+
+    delete[] result;
+
+    return 0;
+
+}
