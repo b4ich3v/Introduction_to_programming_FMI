d4021049318db5a676dd455b091f6b7a35e74d80
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-08-03 00:46:35 +0300

    Update exercise7.cpp

--- Exams/2020-2021-corrective/exercise7.cpp
+++ Exams/2020-2021-corrective/exercise7.cpp
@@ -1,1 +1,65 @@
+#include <iostream>
+
+int sizeOfString(char* string)
+{
+
+    int size = 0;
+
+    while (*string != '\0')
+    {
+
+        size++;
+        string++;
+
+    }
+
+    return size;
+
+}
+
+long convertNumber(char* num) 
+{
+
+    if (num == nullptr) 
+    {
+
+        return -1;
+
+    }
+
+    long sum = 0;
+    int length = sizeOfString(num);
+
+    for (int i = 0; i < length; i++) 
+    {
+
+        if (num[i] < '0' || num[i] > '9') 
+        {
+
+            return -1;  
+
+        }
+
+        sum += num[i];  
+
+    }
+
+    return sum;
+
+}
+
+int main() 
+{
+
+    char num1[] = "314";
+    char num2[] = "442a";
+    char num3[] = "5462141";
+
+    std::cout << convertNumber(num1) << std::endl;  
+    std::cout << convertNumber(num2) << std::endl;  
+    std::cout << convertNumber(num3) << std::endl;  
+
+    return 0;
+
+}
 
