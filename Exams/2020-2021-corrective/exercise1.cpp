6fe5ff55976f15188d51d961c9bdbc5a551d99b0
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-08-02 22:16:57 +0300

    Update exercise1.cpp

--- Exams/2020-2021-corrective/exercise1.cpp
+++ Exams/2020-2021-corrective/exercise1.cpp
@@ -1,1 +1,76 @@
+#include <iostream>
+
+int largestSeq(int array[] ,int N)
+{
+
+    if (array == nullptr || N < 0 )
+    {
+
+        return -1;
+
+    }
+
+    int maxSum = 0;
+    int currSum = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+
+        while (array[i] % 2 == 0)
+        {
+
+            currSum += array[i];
+            i++;
+
+        }
+        
+        if (maxSum < currSum)
+        {
+
+            maxSum = currSum;
+
+        }
+
+        currSum = 0;
+
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+
+        while (array[i] % 2 == 0)
+        {
+
+            currSum += array[i];
+            i++;
+
+        }
+
+        if (maxSum == currSum)
+        {
+
+            return maxSum;
+            break;
+
+        }
+
+        currSum = 0;
+
+    }
+
+    return -1;
+
+}
+
+int main()
+{
+
+    int array[] = { 2, 4, 3, 11, 12, 168, 144, 5, 9, 62, 98, 1 };
+    int N = 12;
+
+    std::cout << largestSeq(array, N);
+
+    return 0;
+
+}
 
