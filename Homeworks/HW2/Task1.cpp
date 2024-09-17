ce26a53e914f0eac473b113fc3fae5c05e03cfb8
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-09-17 16:28:36 +0300

    Update Task1.cpp

--- Homeworks/HW2/Task1.cpp
+++ Homeworks/HW2/Task1.cpp
@@ -23,28 +23,37 @@ void codingFunc(char* str, char* coded) // It is used to encode the character st
 
     while (str[indexOfStr] != '\0')
     {
+
         char currentChar = str[indexOfStr];
 
         for (int k = 7; k >= 0; k--) // Eight block.
         {
+
             if ((currentChar & (1 << k)) != 0)
             {
+
                 coded[indexOfCoded] = '1';
                 coded[indexOfCoded + 1] = '0';
+
             }
             else
             {
+
                 coded[indexOfCoded] = '0';
                 coded[indexOfCoded + 1] = '1';
+
             }
 
             indexOfCoded = indexOfCoded + 2;
+
         }
 
         indexOfStr++;
+
     }
 
     coded[indexOfCoded] = '\0';
+
 
 }
 
@@ -53,7 +62,9 @@ int dimOfString(char* str, int length)
 
     while (str[length] != '\0') // It is used to find the length of a character string by traversing it until the end (null terminator) is reached.
     {
+
         length++;
+
     }
 
     return length;
@@ -68,24 +79,29 @@ void toSymbol(char* binaryNumber)
 
     for (int i = 0; i < length; i = i + 8)
     {
+
         char codedElementValue = 'a';
 
         for (int j = 0; j < 8 && (i + j) < length; j++) // The binary string is divided into blocks of 8 bits each, which are converted to symbols and output.
         {
+
             codedElementValue = (codedElementValue << 1) | (binaryNumber[i + j] - '0');
+
         }
 
         std::cout << codedElementValue;
+
     }
 
 }
 
 int main()
 {
+
     const int eightBlockNumber = 8;
     char str[MAX_LENGTH + 1] = {};
     char* coded = new char[MAX_LENGTH * eightBlockNumber * 2 + 1]; // Declare a dynamic array coded of sufficient size
-                                                                   // to store the binary representation of all characters in str.
+    // to store the binary representation of all characters in str.
     std::cin.getline(str, MAX_LENGTH + 1);
 
     codingFunc(str, coded);
